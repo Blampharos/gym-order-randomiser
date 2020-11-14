36a09377ee6513219334846882445390c205f88d
Blampharos <Blampharos@users.noreply.github.com>
2020-11-14 13:49:46 +0100

    Add JSON loading for unlock tables
    Temporary inline data has been removed in favour of the new JSON data.

--- encounter_generator.py
+++ encounter_generator.py
@@ -2,71 +2,51 @@
 
 from pprint import PrettyPrinter
 from collections import namedtuple
+from json import load
 
 pp = PrettyPrinter(indent=2)
 
 Location = namedtuple("Location", "name extrareq")
 UnlockTable = namedtuple("UnlockTable", "opener locations")
 
-default_unlocks = UnlockTable(
-    "default",
-    (
-        Location( "Route101Land", set(()) ),
-        Location( "Route102Land", set(()) ),
-    )
-)
+def as_unlock_table(dict_):
+    """Object hook for JSON decoder
 
-waterfall_unlocks = UnlockTable(
-    "waterfall",
-    (
-        Location("VictoryRoadLand", set(()) ),
+    Example UnlockTable produced:
+    UnlockTable(
+        "rock_smash",
+        (
+            Location("Route110Land, set() ),
+            Location("Route118Land, set(("acro_bike",)) )
+        )
     )
-)
+    """
+    if "opener" in dict_:
+        return UnlockTable(
+            dict_["opener"],
+            tuple(dict_["locations"])
+        )
+    elif "extrareq" in dict_:
+        return Location( dict_["name"], set( dict_["extrareq"] ) )
+    return dict_
 
-rock_smash_unlocks = UnlockTable(
-    "rock_smash",
-    (
-        Location("Route110Land", set(()) ),
-        Location("Route117Land", set(()) ),
-        Location("Route118Land", set(("acro_bike",)) ),
-    )
-)
+unlock_tables_file = open("unlock_tables.json")
+json_unlock_tables = load(unlock_tables_file, object_hook=as_unlock_table)
 
-acro_bike_unlocks = UnlockTable(
-    "acro_bike",
-    (
-        Location("Route110Land", set(()) ),
-        Location("Route118Land", set(("rock_smash",)) ),
-        Location("Route121Land", set(()) ),
-    )
-)
-
-surf_unlocks = UnlockTable(
-    "surf",
-    (
-        Location("Route110Land", set(()) ),
-        Location("Route110Water", set(("rock_smash",)) ),
-        Location("Route120Water", set(("acro_bike",)) ),
-    )
-)
+#pp.pprint(json_unlock_tables["rock_smash_unlocks"])
 
 unlock_tables = (
-    #default_unlocks,
-    waterfall_unlocks,
-    rock_smash_unlocks,
-    acro_bike_unlocks,
-    surf_unlocks,
+    #json_unlock_tables["default_unlocks"],
+    json_unlock_tables["waterfall_unlocks"],
+    json_unlock_tables["rock_smash_unlocks"],
+    json_unlock_tables["acro_bike_unlocks"],
+    json_unlock_tables["surf_unlocks"],
 )
 
 visit_order = [1, 2, 3, 0]
 
 location_tiers = []
 unlockers_added = set()
-
-#pp.pprint(default_unlocks[1])
-#pp.pprint(rock_smash_unlocks[1:])
-
-pp.pprint(surf_unlocks.opener)
 
 for i in range(len(visit_order)):
     location_tiers.append([])
@@ -80,5 +60,5 @@ for i in range(len(visit_order)):
             location_tiers[i].append(location.name)
     pass
 
-#pp.pprint(location_tiers)
+pp.pprint(location_tiers)
 
