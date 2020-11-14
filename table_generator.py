515e93d2e8fd2aa1175e0c359908bd179c863c06
Blampharos <Blampharos@users.noreply.github.com>
2020-11-14 23:17:55 +0100

    Make location file a dict/object instead
    It's not a file quite yet but whatever. Also there are offsets rather
    than section lengths because order disappears when objects get parsed
    into dicts. This is probably a good change regardless of that.

--- table_generator.py
+++ table_generator.py
@@ -20,44 +20,61 @@ template_encounters = load(template_encounters_file)
 
 encounter_fields = template_encounters["wild_encounter_groups"][0]["fields"]
 
-land_mons_count = len(encounter_fields[0]["encounter_rates"])
-water_mons_count = len(encounter_fields[1]["encounter_rates"])
-rock_smash_mons_count = len(encounter_fields[2]["encounter_rates"])
+mon_size = 4
+land_mons_count = len(encounter_fields[0]["encounter_rates"])*mon_size
+water_mons_count = len(encounter_fields[1]["encounter_rates"])*mon_size
+rock_smash_mons_count = len(encounter_fields[2]["encounter_rates"])*mon_size
 fishing_mons_count = len(encounter_fields[3]["encounter_rates"]) #unused
-old_rod_mons_count = len(encounter_fields[3]["groups"]["old_rod"])
-good_rod_mons_count = len(encounter_fields[3]["groups"]["good_rod"])
-super_rod_mons_count = len(encounter_fields[3]["groups"]["super_rod"])
+old_rod_mons_count = len(encounter_fields[3]["groups"]["old_rod"])*mon_size
+good_rod_mons_count = len(encounter_fields[3]["groups"]["good_rod"])*mon_size
+super_rod_mons_count = len(encounter_fields[3]["groups"]["super_rod"])*mon_size
 
 # Reads every second encounter table, the odd ones are Sapphire
 ruby_maps = template_encounters["wild_encounter_groups"][0]["encounters"][::2]
 
+info_size = 8 # Encounter rate and memory address, we're skipping it
 locations = []
+offset_count = 0
 for i in range(len(ruby_maps)):
     base_label = ruby_maps[i]["base_label"][:-5] # [:-5] Removes "_Ruby"
     if "land_mons" in ruby_maps[i]:
         locations.append(( "{}_LandMons".format(base_label),
-                           land_mons_count, () ))
+                           (offset_count, ()) ))
+        offset_count += land_mons_count + info_size
     if "water_mons" in ruby_maps[i]:
         locations.append(( "{}_WaterMons".format(base_label),
-                           water_mons_count, () ))
+                           (offset_count, ()) ))
+        offset_count += water_mons_count + info_size
     if "rock_smash_mons" in ruby_maps[i]:
         locations.append(( "{}_RockSmashMons".format(base_label),
-                           rock_smash_mons_count, () ))
+                           (offset_count, ()) ))
+        offset_count += rock_smash_mons_count + info_size
     if "fishing_mons" in ruby_maps[i]:
         locations.append(( "{}_OldRodMons".format(base_label),
-                           old_rod_mons_count, () ))
+                           (offset_count, ()) ))
+        offset_count += old_rod_mons_count
         locations.append(( "{}_GoodRodMons".format(base_label),
-                           good_rod_mons_count, () ))
+                           (offset_count, ()) ))
+        offset_count += good_rod_mons_count
         locations.append(( "{}_SuperRodMons".format(base_label),
-                           super_rod_mons_count, () ))
+                           (offset_count, ()) ))
+        offset_count += super_rod_mons_count + info_size
 
 location_file = StringIO()
-location_file.write("[\n")
+location_file.write("{\n")
 for i in range(len(locations)):
     comma = ","
     if(i == len(locations) - 1):
         comma = ""
-    location_file.write("  {}{}\n".format(dumps(locations[i]), comma))
-location_file.write("]")
+    #location_file.write("  {}{}\n".format(dumps(locations[i]), comma))
+    location_file.write('  "{}": {}{}\n'.format( locations[i][0],
+                                                 dumps(locations[i][1]),
+                                                 comma ))
+location_file.write("}\n")
 
 print(location_file.getvalue())
+
+location_file.seek(0)
+locations = load(location_file)
+
+#pp.pprint(locations)
